9afba3698f43b902f845c48210085328bc850799
k9k8j7y7-jpg <k9k8j7y7-jpg@users.noreply.github.com>
2026-03-09 09:28:42 +0900

    Deploy: Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/detail/kera_manual/',
+  base: '/kerax_use/',
 })
